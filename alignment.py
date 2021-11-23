cc4544a38d4d9c7c0b38635ed9833630150892a6
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2021-11-23 10:05:35 -0500

    Added functions to transform from global coordinates to mirror coordinates

--- alignment.py
+++ alignment.py
@@ -1,5 +1,6 @@
 import adjustments as adj
 import mirror_fit as mf
+import numpy as np
 
 """
 File structure:
@@ -31,3 +32,52 @@ Workflow:
     Fit for adjustments with adj
     Print out adjustments and save to a file in root of measurement dir
 """
+
+v_m1 = (0, 0, 3600) #mm
+v_m2 = (0, -4800, 0) #mm
+a_m1 = -np.arctan(0.5)
+a_m2 = np.arctan(1./3.) - np.pi/2
+
+def global_to_mirror(coords, shift, v_m, a_m):
+    """
+    Transform from global coordinates to mirror coordinates
+
+    @param coords: Array of points to transform
+    @param shift: Origin shift in mm (in addition to the standard one for the mirror)
+    @param v_m: Standard origin shift for the mirror
+    @param a_m: Angle for coordinate rotation (reffered to as alpha in Vertex docs)
+
+    @return m_coords: The points in the mirror coords
+    """
+    shifted_coords = coords - shift - v_m
+    rot_mat = np.array([[1., 0., 0.],
+                        [0., np.cos(a_m), np.sin(a_m)],
+                        [0., -np.sin(a_m), np.cos(a_m)]])
+    m_coords = np.zeros(shifted_coords.shape)
+    for i,point in enumerate(shifted_coords):
+        m_coords[i] = rot_mat @ point
+    return m_coords
+
+
+def global_to_primary(coords, shift):
+    """
+    Transform from global coordinates to primary mirror coordinates
+
+    @param coords: Array of points to transform
+    @param shift: Origin shift in mm (in addition to the standard one for the mirror)
+
+    @return m_coords: The points in the primary mirror coords
+    """
+    return global_to_mirror(coords, shift, v_m1, a_m1)
+
+
+def global_to_secondary(coords, shift):
+    """
+    Transform from global coordinates to secondary mirror coordinates
+
+    @param coords: Array of points to transform
+    @param shift: Origin shift in mm (in addition to the standard one for the mirror)
+
+    @return m_coords: The points in the secondary mirror coords
+    """
+    return global_to_mirror(coords, shift, v_m2, a_m2)
