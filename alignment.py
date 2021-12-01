d5a92c5ae308c3cbdebb8b8a753f142cc1c00b3d
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2021-12-01 15:03:55 -0500

    Added more coordinate transforms and fixed which mirror calls which transform

--- alignment.py
+++ alignment.py
@@ -81,6 +81,81 @@ def global_to_secondary(coords, shift):
     return global_to_mirror(coords, shift, v_m2, a_m2)
 
 
+def mirror_to_global(coords, shift, v_m, a_m):
+    """
+    Transform from mirror coordinates to global coordinates
+
+    @param coords: Array of points to transform
+    @param shift: Origin shift in mm (in addition to the standard one for the mirror)
+    @param v_m: Standard origin shift for the mirror
+    @param a_m: Angle for coordinate rotation (reffered to as alpha in Vertex docs)
+
+    @return m_coords: The points in global coords
+    """
+    shifted_coords = coords - shift
+    rot_mat = np.array(
+        [
+            [1.0, 0.0, 0.0],
+            [0.0, np.cos(a_m), -np.sin(a_m)],
+            [0.0, np.sin(a_m), np.cos(a_m)],
+        ]
+    )
+    m_coords = np.zeros(shifted_coords.shape)
+    for i, point in enumerate(shifted_coords):
+        m_coords[i] = rot_mat @ point
+    return m_coords + v_m
+
+
+def primary_to_global(coords, shift):
+    """
+    Transform from primary mirror coordinates to global coordinates
+
+    @param coords: Array of points to transform
+    @param shift: Origin shift in mm (in addition to the standard one for the mirror)
+
+    @return m_coords: The points in global mirror coords
+    """
+    return mirror_to_global(coords, shift, v_m1, a_m1)
+
+
+def secondary_to_global(coords, shift):
+    """
+    Transform from secondary mirror coordinates to global coordinates
+
+    @param coords: Array of points to transform
+    @param shift: Origin shift in mm (in addition to the standard one for the mirror)
+
+    @return m_coords: The points in global mirror coords
+    """
+    return mirror_to_global(coords, shift, v_m2, a_m2)
+
+
+def primary_to_secondary(coords, shift):
+    """
+    Transform from primary mirror coordinates to secondary mirror coordinates
+
+    @param coords: Array of points to transform
+    @param shift: Origin shift in mm (in addition to the standard one for the mirror)
+
+    @return m_coords: The points in secondary mirror coords
+    """
+    global_coords = primary_to_global(coords, shift)
+    return global_to_secondary(global_coords, 0)
+
+
+def secondary_to_primary(coords, shift):
+    """
+    Transform from secondary mirror coordinates to primary mirror coordinates
+
+    @param coords: Array of points to transform
+    @param shift: Origin shift in mm (in addition to the standard one for the mirror)
+
+    @return m_coords: The points in primary mirror coords
+    """
+    global_coords = secondary_to_global(coords, shift)
+    return global_to_primary(global_coords, 0)
+
+
 def shift_coords(coords, shift):
     """
     Apply origin shift to coordinate
@@ -272,7 +347,7 @@ if os.path.exists(primary_path):
     elif coordinates is "primary":
         coord_trans = shift_coords
     else:
-        coord_trans = primary_to_secondary
+        coord_trans = secondary_to_primary
 
     # Align panels
     align_panels(panels, primary_path, out_file, coord_trans, origin_shift, mf.primary_fit_func)
@@ -293,7 +368,7 @@ if os.path.exists(secondary_path):
     elif coordinates is "secondary":
         coord_trans = shift_coords
     else:
-        coord_trans = secondary_to_primary
+        coord_trans = primary_to_secondary
 
     # Align panels
     align_panels(panels, secondary_path, out_file, coord_trans, origin_shift, mf.secondary_fit_func)
