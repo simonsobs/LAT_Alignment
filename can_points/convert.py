ebc894ba99f8db4777a43cf2d149f122590e2e6f
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2021-12-10 12:39:05 -0500

    Added script to convert csv of adjustor positions to text files that the alignmenr script understands

---
+++ can_points/convert.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import argparse as ap
+
+# Parse command line arguments
+parser = ap.ArgumentParser(
+    description="Convert CSV of vertex adjustor spreadsheet to text file readable by alignment script"
+)
+parser.add_argument("path", help="Path to file to convert")
+args = parser.parse_args()
+path = args.path
+
+pts = np.genfromtxt(path, dtype=str, skip_header=2, delimiter=',')
+
+blank = np.zeros(pts.shape[1], dtype=str)
+mask = ~(pts == blank).all(1)
+pts = pts[mask]
+
+col_mask = np.ones(pts.shape[1], dtype=bool)
+col_mask[[0, 6, 7]] = False
+pts = pts[:, col_mask]
+
+copy = np.where(pts[:,4] == "-->")[0]
+pts[copy, 2:5] = pts[copy, 7:10]
+
+copy = np.where(pts[:,7] == "<--")[0]
+pts[copy, 7:10] = pts[copy, 2:5]
+
+new_pts = np.zeros((2*pts.shape[0], int(pts.shape[1]/2)), dtype='<U12')
+new_pts[:pts.shape[0]] = pts[:, :new_pts.shape[1]]
+new_pts[pts.shape[0]:] = pts[:, new_pts.shape[1]:]
+
+
+blank = np.zeros(new_pts.shape[1], dtype=str)
+mask = ~(new_pts == blank).all(1)
+pts = new_pts[mask]
+
+new_path = os.path.splitext(path)[0] + '.txt'
+np.savetxt(new_path, pts, fmt='%s')
