40b919e4fdada82138b4ba909e6c2c9220b81175
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2023-07-01 22:13:31 -0400

    Specify package

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="lat_alignment",
     version="1.0.0",
+    packages=["lat_alignment"],
     install_requires=["scipy", "numpy", "matplotlib", "pyyaml"],
     entry_points={"console_scripts": ["lat_alignment=lat_alignment.alignment:main"]},
 )
