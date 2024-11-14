e1548628eded289d9c3d3df73fa056302ea73739
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2024-11-14 12:58:30 -0500

    ci+build: setup build and ci tools and switch to pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="lat_alignment",
-    version="3.0.0",
-    packages=["lat_alignment"],
-    install_requires=["scipy", "numpy", "matplotlib", "pyyaml", "megham", "pqdm"],
-    entry_points={"console_scripts": ["lat_alignment=lat_alignment.alignment:main"]},
-)
