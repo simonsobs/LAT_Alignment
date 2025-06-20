4874a3b11ea03950ed687cc7aefcf7a702ab0e8c
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2025-06-20 03:39:28 +0200

    chore: add some on the fly scripts for aligning with tracker

---
+++ scratch/laser_alignment/laser_m2_full.py
@@ -0,0 +1,164 @@
+# coding: utf-8
+import megham.transform as mt
+import numpy as np
+m1 = np.array([2416.614119,-6595.836727,4813.948184],[-2382.073056,-6595.759455,4830.627112],[2405.124325,-1238.157415,2141.693447],[-2392.537523,-1236.687137,2156.648307]])
+m1 = np.array([[2416.614119,-6595.836727,4813.948184],[-2382.073056,-6595.759455,4830.627112],[2405.124325,-1238.157415,2141.693447],[-2392.537523,-1236.687137,2156.648307]])
+m2 = np.array([[-2005.078291,-7651.933572,-2543.164877],[1987.258081,-7652.457738,-2555.584845],[2008.112123,-9391.239271,2646.833902],[-1986.072995,-9390.194963,2657.575995]])
+m1_nom = np.array([[2400.94, -2698.56, 4819.33],[-2397.25, -2698.83, 4821.03],[2397.31, 2656.61, 2142.25],[-2399.23, 2658.4, 2141.8]])
+m2_nom = np.array([[-1998.53, -3762.8, -2550.87],[1993.61, -3763.22, -2551.27],[1998.7, -5497.53, 2652.13],[-1995.38, -5496.5, 2651.13]])
+aff, sft = mt.get_rigid(m2_nom, m2, method='mean)
+aff, sft = mt.get_rigid(m2_nom, m2, method='mean')
+m1_exp = mt.apply_transform(m1_nom, aff, aft)
+m1_exp = mt.apply_transform(m1_nom, aff, sft)
+m1_exp - m1
+aff2, sft2 = mt.get_rigid(m1_exp, m1, method='mean')
+rot2 = np.rad2deg(mt.decompose_rotation(aff2))
+rot2
+sft2
+m1_exp - m1
+m1
+rot2
+m1_pred = mt.apply_transform(m1_exp, aff2, sft2)
+m1_pred-m1
+rot2
+.0078/57 * 6000
+rot
+rot = np.rad2deg(mt.decompose_rotation(aff))
+rot
+sft
+aff_m1, sft_m1 = mt.get_rigid(m1, m1_nom, method='mean')
+m2_exp = mt.apply_transform(m2, aff_m1, sft_m1)
+aff_m2, sft_m2 = mt.get_rigid(m2_exp, m2_nom, method='mean')
+np.rad2deg(mt.decompose_rotation(aff_m2))
+sft_m2
+sft_m1
+sft
+m1 - m1_nom
+(m1 - m1_nom)[:, [True, False, True]]
+np.linalg.norm((m1 - m1_nom)[:, [True, False, True]])
+np.linalg.norm((m2 - m2_nom)[:, [True, False, True]])
+m2 - m2_nom
+np.linalg.norm((m2 - m2_nom)[:, [True, False, True]], axis=-1)
+sft
+sft_m1
+sft_m1 + sft
+m2 - m2_nom
+m1 - m1_nom
+(m1 - m1_nom) - (m2 - m2_nom)
+aff_m2, sft_m2 = mt.get_rigid(m2, m2_nom, method='mean')
+rot_m1 = np.rad2deg(mt.decompose_rotation(aff_m1))
+rot_m2 = np.rad2deg(mt.decompose_rotation(aff_m2))
+sft_m1
+sft_m2
+sft_m1 - sft_m2
+rot_m1 - rot_m2
+rot_m1
+rot_m2
+y_cm_sft = np.array([0, (sft_m1[1] + sft_m2[1])/2, 0])
+y_cm_rot = np.array([0, (rot_m1[1] + rot_m2[1])/2, 0])
+sft_m1 - y_cm_sft
+rot_m1 - y_cm_rot
+sft_m2 - y_cm_sft
+rot_m2 - y_cm_rot
+m1_nom
+m1_nom
+m2_nom
+m1
+m2
+get_ipython().run_line_magic('pinfo', 'mt.get_rigid')
+aff, sft = mt.get_affine(m1, m1_nom, method='mean')
+sft
+aff, sft = mt.get_affine(m1, m1_nom, method='mean', weights=np.ones(len(m1)))
+sft
+aff_m1, sft_m1 = mt.get_rigid(m1, m1_nom, method='mean')
+sft_m1
+aff_m1, sft_m1 = mt.get_rigid(m1, m1_nom, method='median')
+sft_m1
+y_cm_sft
+aff_m1, sft_m1 = mt.get_rigid(m1 - y_cm_sft, m1_nom, method='median')
+sft_m1
+aff_m1, sft_m1 = mt.get_rigid(m1 - y_cm_sft, m1_nom, method='mean')
+sft_m1
+aff_m1, sft_m1 = mt.get_rigid(m1 + y_cm_sft, m1_nom, method='median')
+sft_m1
+y_cm_sft
+y_cm = np.array([0, 4410, 0])
+aff_m1, sft_m1 = mt.get_rigid(m1 + y_cm, m1_nom, method='median')
+sft_m1
+aff_m1, sft_m1 = mt.get_rigid(m1 - y_cm, m1_nom, method='median')
+sft_m1
+y_cm = np.array([0, 4360, 0])
+aff_m1, sft_m1 = mt.get_rigid(m1 + y_cm, m1_nom, method='median')
+sft_m1
+y_cm = np.array([0, 4410-545, 0])
+aff_m1, sft_m1 = mt.get_rigid(m1 + y_cm, m1_nom, method='median')
+sft_m1
+y_cm = np.array([0, 4410-545-25.4, 0])
+aff_m1, sft_m1 = mt.get_rigid(m1 + y_cm, m1_nom, method='median')
+sft_m1
+y_cm = np.array([0, 4410-545+25.4, 0])
+aff_m1, sft_m1 = mt.get_rigid(m1 + y_cm, m1_nom, method='median')
+sft_m1
+sft_m2
+aff_m1, sft_m1 = mt.get_rigid(m1 + y_cm, m1_nom, method='mean')
+sft_m1
+aff_m1, sft_m1 = mt.get_affine(m1 + y_cm, m1_nom, method='mean')
+sft_m1
+aff_m1, sft_m1 = mt.get_rigid(m1 + y_cm, m1_nom, method='mean')
+aff_m2, sft_m2 = mt.get_rigid(m2 + y_cm, m2_nom, method='mean')
+sft_m2
+m1 + y_cm
+m1
+(m1 + y_cm) - m1_nom
+(m2 + y_cm) - m2_nom
+m2
+m1_nom - mt.apply_transform(m1 + y_cm, aff_m1, sft_m1)
+rot_m1 = np.rad2deg(mt.decompose_rotation(aff_m1))
+rot_m1
+sft_m1
+rot_m2 = np.rad2deg(mt.decompose_rotation(aff_m2))
+rot_m2
+m2_nom - mt.apply_transform(m2 + y_cm, aff_m1, sft_m1)
+m2_nom - mt.apply_transform(m2 + y_cm, aff_m1, sft_m2)
+m2_nom - mt.apply_transform(m2 + y_cm, aff_m2, sft_m2)
+y_rot = (rot_m1[1] - rot_m2[1])
+from scipy.spatial.transform import Rotation as R
+get_ipython().run_line_magic('pinfo', 'R.from_euler')
+r = R.from_euler("Y", [y_rot])
+r
+r.apply(m1)
+r.apply(m1) - m1_nom
+r = R.from_euler("Y", [-y_rot])
+r.apply(m1) - m1_nom
+r = R.from_euler("Y", [y_rot], degrees=True)
+r.apply(m1) - m1_nom
+m1 - m1_nom
+r = R.from_euler("Y", [-y_rot], degrees=True)
+r.apply(m1) - m1_nom
+y_rot
+y_rot = (rot_m1[1] + rot_m2[1])/2
+r = R.from_euler("Y", [y_rot], degrees=True)
+r.apply(m1) - m1_nom
+r = R.from_euler("Y", [-y_rot], degrees=True)
+r.apply(m1) - m1_nom
+m1 - m1_nom
+aff_m1, sft_m1 = mt.get_rigid(r.apply(m1) + y_cm, m1_nom, method='mean')
+sft_m1
+rot_m1
+rot_m1 = np.rad2deg(mt.decompose_rotation(aff_m1))
+rot_m1
+sft_m1
+m2
+m2_nom
+y_cm
+4410 - 545
+sft_m2
+rot_m2
+m2 - y_cm
+m2 + y_cm
+sft_m2
+y_cm
+sft_m1
+rot_m1
+rot_m2
+m2_nom
---
+++ scratch/laser_alignment/latr_from_m2_aligned.py
@@ -0,0 +1,55 @@
+
+import megham.transform as mt
+import numpy as np
+latr_nom = np.array([[-1030.0000046587363, 6996.4, -179.99997401227625], [-359.1154521541986, 6996.4, 982.0061569386937], [359.11545215419835, 6996.4, 982.0061569386938], [1030.0000046587363, 6996.4, -179.99997401227645]])
+latr_pt_5 = latr_nom[2]
+latr_pt_5[2] *= -1
+latr_nom = np.vstack([latr_nom[[True, False, True, True]], latr_pt_5])
+latr_nom
+latr_1 = np.array([[-1031.0669, 6996.39, -191.1107], [341.44298, 6987.692, 990.4713], [1028.9343, 6985.7814, -161.7898], [-348.7398, 6993.8709, -983.7343])
+latr_1 = np.array([[-1031.0669, 6996.39, -191.1107], [341.44298, 6987.692, 990.4713], [1028.9343, 6985.7814, -161.7898], [-348.7398, 6993.8709, -983.7343]])
+aff_l, sft_; = mt.get_rigid(latr_1, latr_nom, method='mean')
+aff_l, sft_l = mt.get_rigid(latr_1, latr_nom, method='mean')
+sft_l
+latr_nom
+latr_q
+latr_1
+latr_nom[-1][0] *= -1
+latr_nom
+latr_nom - latr_1
+latr_nom[1][2] *= -1
+latr_nom - latr_1
+aff_l, sft_l = mt.get_rigid(latr_1, latr_nom, method='mean')
+rot_l = np.rad2deg(mt.decompose_rotation(aff_l))
+sft_l
+rot_l
+latr_2 = np.array([[-1033.955, 6996.382, -176.78], [355.286, 6987.606, 985.385], [1026.4807, 6985.822, -176.471], [-362.497, 6993.902, -978.791]])
+aff_l, sft_l = mt.get_rigid(latr_2, latr_nom, method='mean')
+sft_l
+latr_nom
+rot_l = np.rad2deg(mt.decompose_rotation(aff_l))
+rot_l
+latr_nom - latr_2
+latr_3 = np.array([[-1033.4623, 6997.069, -176.876], [355.611, 6995.1213, 985.1908], [1026.819, 6995.516, -176.654], [-362.4245, 6997.8483, -979.0334]])
+aff_l, sft_l = mt.get_rigid(latr_2, latr_nom, method='mean')
+rot_l = np.rad2deg(mt.decompose_rotation(aff_l))
+rot_l
+aff_l, sft_l = mt.get_rigid(latr_3, latr_nom, method='mean')
+rot_l = np.rad2deg(mt.decompose_rotation(aff_l))
+rot_l
+latr_nom - latr_3
+latr_4 = np.array([[-1033.1582, 6995.97, -176.4853], [356.0597, 6994.637, 985.5317], [1027.1206, 6995.4984, -176.393], [-362.1488, 6996.3764, -978.699]])
+aff_l, sft_l = mt.get_rigid(latr_4, latr_nom, method='mean')
+rot_l
+rot_l = np.rad2deg(mt.decompose_rotation(aff_l))
+rot_l
+latr_nom - latr_4
+sft_l
+latr_5 = np.array([[-1030.557, 6996.200, -178.308], [359.08, 6996.6566, 983.343], [1029.946, 6997.0637, -178.75], [-359.5163, 6996.426, -980.6245]])
+latr_6 = np.array([[-1030.557, 6996.200, -178.308], [359.08, 6996.6566, 983.343], [1029.946, 6997.0637, -178.75], [-359.5163, 6996.426, -980.6245]])
+aff_l, sft_l = mt.get_rigid(latr_6, latr_nom, method='mean')
+rot_l = np.rad2deg(mt.decompose_rotation(aff_l))
+sft_l
+sft_l
+rot_l
+latr_nom - latr_6
